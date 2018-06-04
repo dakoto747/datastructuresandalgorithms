4ccc35abe85b903956d7457fd94e40ce2f0af2c7
David Toluhi <dakoto747@gmail.com>
2018-06-04 13:28:49 +0100

    added tips for language mastering

---
+++ src/main/java/com/dakoto/datastructuresandalgorithms/LanguageMastering.java
@@ -0,0 +1,22 @@
+package com.dakoto.datastructuresandalgorithms;
+
+public class LanguageMastering {
+
+    /**
+     *  1. understand syntax
+     *  2. understand control (for, while, dowhile)
+     *  3. understand main boilerplate(int main, public static void main, if __name__ == "__main__": )
+     *  4. understand primitives (int, char, bool, object, prototypes, etc
+     *  5. understand Collections: arrays, lists, hashmaps, tuples, vectors,
+     *  6. understand oop, methods, polymorphism, etc
+     *  7. understand references (pass by value, etc)
+     *  8. understand pointers
+     *  9. understand garbage collection
+     * 10. understand packages, libraries, headers, etc.
+     * 11. understand how to browse standard library documentation (javadocs, std classes, etc)
+     * 12. understand exception handling
+     * 12. understand memory layout(is it column-major, is it row major?)
+     * 13. understand concurrent/parallel programming and libraries(if available)
+     */
+
+}
--- src/main/java/com/dakoto/datastructuresandalgorithms/datastructures/Trees/Tree.java
+++ src/main/java/com/dakoto/datastructuresandalgorithms/datastructures/Trees/Tree.java
@@ -16,5 +16,4 @@ public interface Tree extends DataStructure {
 
     void getHeight();
 
-   
 }
