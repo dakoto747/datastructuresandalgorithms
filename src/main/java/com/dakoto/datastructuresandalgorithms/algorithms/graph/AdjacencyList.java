d4b73da322b6bebb0ebf5fa725f7865ec33fbe51
David Toluhi <dakoto747@gmail.com>
2018-06-13 08:35:20 +0100

    added graph and few more tips

---
+++ src/main/java/com/dakoto/datastructuresandalgorithms/algorithms/graph/AdjacencyList.java
@@ -0,0 +1,20 @@
+package com.dakoto.datastructuresandalgorithms.algorithms.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AdjacencyList<T> {
+//use examples: bank structure as graph, prefix tree, etc.
+    // routes, friends, etc on linkedlist
+    //always use examples! application based learning
+
+    private HashMap<T, ArrayList<T>> adjancencyList = new HashMap<>();
+
+    public void addNode(){
+
+    }
+
+    public void deleteNode(){
+
+    }
+}
--- src/main/java/com/dakoto/datastructuresandalgorithms/algorithms/search/Search.java
+++ src/main/java/com/dakoto/datastructuresandalgorithms/algorithms/search/Search.java
@@ -7,7 +7,10 @@ public interface Search<T> {
 
     default T depthFirstSearch(){
         /*
-        Note that pre-order and other forms of tree traversal are a form of DFS. The key difference is that when implementing this algorithm for a graph, we must check if the node has been visited. If we don't, we risk getting stuck in an infinite loop.
+        Note that pre-order and other forms of tree traversal are a form of DFS.
+        The key difference is that when implementing this algorithm for a graph,
+        we must check if the node has been visited.
+        If we don't, we risk getting stuck in an infinite loop.
 
          */
         return null;
